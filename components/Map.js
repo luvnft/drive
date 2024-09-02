9981d290e6dbc2535aec543f76410f0e2a63e28f
The Wizard of Hahz <info@luvnft.com>
2024-09-01 20:53:24 -0400

    updated

--- components/Map.js
+++ components/Map.js
@@ -1,49 +1,55 @@
-import { useEffect, useContext } from 'react'
-import mapboxgl from 'mapbox-gl'
-import { UberContext } from '../context/uberContext'
+import { useEffect, useContext } from 'react';
+import mapboxgl from 'mapbox-gl';
+import { UberContext } from '../context/uberContext';
 import MapboxDirections from '@mapbox/mapbox-sdk/services/directions';
-
-const directionsService = MapboxDirections({
-  accessToken: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
-});
 
 const style = {
   wrapper: `flex-1 h-full w-full absolute bottom-0`,
-}
-
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+};
 
 const Map = () => {
-  const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext)
+  const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext);
 
   useEffect(() => {
+    const accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+    
+    if (!accessToken) {
+      console.error("Mapbox access token is missing. Please set NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN in your environment variables.");
+      return;
+    }
+
+    mapboxgl.accessToken = accessToken;
+
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/coderweb3/clgqlo13q00jk01qy2cgb77z8',
-      center: [-.2, 39.39],
-      zoom: 4,
-    })
+      center: [-84.3880, 33.7490], // Centered on Atlanta, Georgia
+      zoom: 10,
+    });
 
     if (pickupCoordinates) {
-      addToMap(map, pickupCoordinates)
+      addToMap(map, pickupCoordinates);
     }
 
     if (dropoffCoordinates) {
-      addToMap(map, dropoffCoordinates)
+      addToMap(map, dropoffCoordinates);
     }
 
     if (pickupCoordinates && dropoffCoordinates) {
       map.fitBounds([dropoffCoordinates, pickupCoordinates], {
         padding: 400,
-      })
+      });
     }
-  }, [pickupCoordinates, dropoffCoordinates])
+
+    return () => map.remove(); // Cleanup on unmount
+
+  }, [pickupCoordinates, dropoffCoordinates]);
 
   const addToMap = (map, coordinates) => {
-    const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
-  }
+    new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+  };
 
-  return <div className={style.wrapper} id='map' />
-}
+  return <div className={style.wrapper} id='map' />;
+};
 
-export default Map
+export default Map;
